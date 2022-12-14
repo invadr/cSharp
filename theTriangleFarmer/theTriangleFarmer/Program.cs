08bd38405dae4062af4040d7e382086fa8a0d8c3
invadr <invadr.game.studios@gmail.com>
2022-12-13 23:46:09 -0600

    C# Player's Guide Exercises

---
+++ theTriangleFarmer/theTriangleFarmer/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What is your triangle's base measurement?");
+        string b = Console.ReadLine();
+        double triangleBase = Convert.ToDouble(b);
+
+        Console.WriteLine("What is your triangle's height measurement?");
+        string h = Console.ReadLine();
+        double triangleHeight = Convert.ToDouble(h);
+
+        double area = triangleBase * triangleHeight / 2;
+        Console.WriteLine($"Your triangle's area is {area} square inches.");
+
+        Console.ReadKey();
+    }
+}
