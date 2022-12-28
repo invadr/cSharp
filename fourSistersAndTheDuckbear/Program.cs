15a67e69d40dde2d12cf14fbbc98c21ce1ed8a6c
invadr <117381171+invadr@users.noreply.github.com>
2022-12-28 12:46:12 -0600

    C# Player's Guide exercise solutions

---
+++ fourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace fourSistersAndTheDuckbear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many chocolate eggs were gathered today?");
+            string eggs = Console.ReadLine();
+            int eggsInt = Convert.ToInt32(eggs);
+
+            int allotment = eggsInt / 4;
+            int leftover = eggsInt % 4;
+
+            Console.WriteLine($"The four sisters get {allotment} eggs each. And Duckbear gets {leftover} eggs.");
+
+
+        }
+    }
+}
