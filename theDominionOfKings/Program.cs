3c03b4881b8633fe780ac58e7d2ec4c8b855ed35
invadr <117381171+invadr@users.noreply.github.com>
2022-12-30 20:36:21 -0600

    new csharp commit

---
+++ theDominionOfKings/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace theDominionOfKings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many provinces does your kingdom have?");
+            int provinces = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many duchies does your kingdom have?");
+            int duchies = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many estates does your kingdom have?");
+            int estates = Convert.ToInt32(Console.ReadLine());
+
+            int points = (provinces * 6) + (duchies * 3) + (estates);
+            Console.WriteLine($"Your kingdom has {points} total points.");
+        }
+    }
+}
